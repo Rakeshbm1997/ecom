f23b29b464c3dacfc6176e20d1225b1610967831
RAKESHBM <bmrakesh7@gmail.com>
2024-01-03 09:21:30 +0530

    second commit

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "../Components/Layouts/Layout";
+
+const Policy = () => {
+  return (
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="https://sharnamfoundation.in/images/resource/privacy-policy.jpg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
---
+++ routes/productRoutes.js
@@ -0,0 +1,70 @@
+import express from "express";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+  productFiltersController,
+  productCountController,
+  productListController,
+  productCategoryController,
+  realtedProductController,
+  searchProductController,
+ 
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router(); 
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/product/:pid", deleteProductController);
+
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//product search
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+export default router
